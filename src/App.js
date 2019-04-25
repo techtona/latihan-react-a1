8a454ead631bdd3dfc4607358e2d9ceec05176c3
Jati Pikukuh <jati.pikukuh@maiil.ugm.ac.id>
2019-04-25 09:01:30 +0700

    install material-ui

--- src/App.js
+++ src/App.js
@@ -1,45 +1,10 @@
 import React, {Component} from 'react';
-// import './App.css';
-import Product from './Product';
+import Navbar from './MyAppBar';
 
 class App extends Component {
     render() {
         return (
-            <div className="row">
-                <div className="col m4">
-                    <h3 className="grey-text">Tambah Produk</h3>
-                    <div className="card">
-                        <form action="#" ref="form">
-                            <div className="card-content">
-                                <div className="row">
-                                    <div className="input-field col s12">
-                                        <input placeholder="Judul" id="judul" type="text"/>
-                                        <label htmlFor="judul">Judul</label>
-                                    </div>
-                                    <div className="input-field col s12">
-                                        <input id="keterangan" placeholder="Keterangan" type="text"/>
-                                        <label htmlFor="keterangan">Keterangan</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="card-action">
-                                <button className="btn waves-effect waves-light" type="submit" name="action">Submit
-                                    <i className="material-icons left">send</i>
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div className="col m8">
-                    <div className="row">
-                        <Product judul="Charger HP" keterangan="Lorem ipsum sit dolor amet"/>
-                        <Product judul="Charger HP" keterangan="Lorem ipsum sit dolor amet"/>
-                        <Product judul="Charger Laptop" keterangan="Lorem ipsum sit dolor amet"/>
-                        <Product judul="Charger Motor" keterangan="Lorem ipsum sit dolor amet"/>
-                    </div>
-                </div>
-            </div>
-
+            <Navbar/>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,43 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import Product from './Product';
-
-/*
-function formatNama(mahasiswa) {
-    return mahasiswa.nama_depan + ' ' + mahasiswa.nama_belakang;
-}
-
-const mhs = {
-    nama_depan : 'Lorem',
-    nama_belakang : 'Ipsum'
-}
-
-// const nama = 'Lorem Ipsum';
-const element = <h3>Hello, namaku : {formatNama(mhs)}</h3>;
-
-ReactDOM.render(element, document.getElementById('root'));
-*/
-
-
-// function tik() {
-//     const element = (
-//         <div>
-//             <h1>Hello, world!</h1>
-//             <h2>It is {new Date().toLocaleTimeString()}.</h2>
-//         </div>
-//     );
-//     ReactDOM.render(element, document.getElementById('root'));
-// }
-//
-// setInterval(tik, 1000);
-
-// function HelloWorld(props){
-//     return <h1>Hello {props.nama} from Component(function)</h1>
-// }
-//
-// ReactDOM.render(<HelloWorld nama="Lorem Ipsum"/>, document.getElementById('root'));
-
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
