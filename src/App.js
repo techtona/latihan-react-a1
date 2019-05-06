2f22df7f7663e44f8c70a7eafc38ebb30e460d40
Jati Pikukuh <jati.pikukuh@maiil.ugm.ac.id>
2019-05-06 09:01:21 +0700

    belajar google maps dan penggunaan state dalam komponen dari library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React, {Component} from 'react';
 import Navbar from './MyAppBar';
+import FoodCard from "./FoodCard";
+import SimpleMap from "./SimpleMap";
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            foods : [
+                {
+                    title : 'Fried rice with Onion',
+                    date : '29 January 2019',
+                    desc : 'Lorem i[sum sit dolor amet'
+                },
+                {
+                    title : 'Fried rice without Onion',
+                    date : '29 January 2019',
+                    desc : 'Lorem i[sum sit dolor amet'
+                }
+            ]
+        }
+    }
     render() {
         return (
-            <Navbar/>
+            <div>
+                <Navbar/>
+                <SimpleMap/>
+                <br/>
+                {this.state.foods.map((data)=>{
+                    return(
+                        <div>
+                            <FoodCard title={data.title} date={data.date} description={data.desc}/>
+                            <br/>
+                        </div>
+                    )
+                })}
+            </div>
+
         );
     }
 }
