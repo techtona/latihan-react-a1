3694b1cf9b4591191f9f4561a56391aa1169ddeb
Jati Pikukuh <jati.pikukuh@maiil.ugm.ac.id>
2019-05-07 08:48:35 +0700

    add simple app bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 import Main from './components/page/Main';
+import MyAppBar from "./components/MyAppBar";
 
 class App extends Component {
     render() {
         return (
             <div>
-                <Link to='/home'>Home</Link>&nbsp;&nbsp;
-                <Link to='/profile'>Profile</Link>&nbsp;&nbsp;
-                <Link to='/portofolio'>Portofolio</Link>&nbsp;&nbsp;
-                <Link to='/contact'>Contact</Link>&nbsp;&nbsp;
+                <MyAppBar/>
                 <hr/>
                 <Main/>
             </div>
